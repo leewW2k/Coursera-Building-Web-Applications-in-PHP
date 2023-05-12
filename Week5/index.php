e25915a2887c08ceb1d2d05ec1c942d511ea7c45
Wei Xian <45598522+leewX2k@users.noreply.github.com>
2023-05-13 01:35:37 +0800

    Add files via upload

---
+++ Week5/index.php
@@ -0,0 +1,26 @@
+<title>
+   #LEE WEI XIAN# PHP
+</title>
+<h1>
+   #LEE WEI XIAN# PHP
+</h1>
+<p>
+   The SHA256 hash of "#LEE WEI XIAN#" is
+   <?php 
+      $name = "#LEE WEI XIAN#" ;
+      echo hash('sha256', $name)
+   ?>
+</p>
+<p>
+   ASCII ART:
+   <pre style="margin-left: 4em">
+   #   # 
+# # # # # #
+   #   # 
+# # # # # #
+   #   #
+   </pre>
+</p>
+
+<a href="check.php"><div>Click here to check the error setting</div></a>
+<a href="fail.php">Click here to cause a traceback</a>
